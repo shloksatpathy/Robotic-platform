544d6cb176a5c0b991fa6477f029611534ecd518
shloksatpathy <shloke1504@gmail.com>
2026-05-22 13:57:08 +0530

    FEAT: Video based testing for the detector

--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,4 @@
-import cv2
+"""import cv2
 from detector import detect
 
 cap = cv2.VideoCapture(0)
@@ -11,5 +11,30 @@ while True:
 
     print(detections)
 
+    cv2.imshow()
+
     if cv2.waitKey(1) & 0xff == ord("q"):
+        break"""
+
+import cv2
+from detector import detect
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
         break
+
+    annotated_frame, detections = detect(frame)
+
+    print(detections)
+
+    cv2.imshow("YOLO Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
